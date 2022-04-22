88da929718a1e42425e5bcbd246ddf3e17800080
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-04-22 11:24:22 +0900

    feat: slider 구현 중 오류 발생

--- FE/src/components/Main/SubContents.js
+++ FE/src/components/Main/SubContents.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { FONT } from '../../constants/fonts';
@@ -20,21 +21,55 @@ const RightArrow = styled.div`
   margin-top: 25%;
   padding-right: 20px;
 `;
-const createSubContent = (content, idx) => (
-  <div key={content + idx}>
-    <SubTitle>
-      <Text font={FONT.XLARGE_BOLD}>{content.title}</Text>
-    </SubTitle>
-    <FoodCardsWrap>
-      <LeftArrow>
-        <LeftArrowIcon />
-      </LeftArrow>
-      <FoodCards foods={content.foods} />
-      <RightArrow>
-        <RightArrowIcon />
-      </RightArrow>
-    </FoodCardsWrap>
-  </div>
-);
-const SubContents = ({ subContents }) => <div>{subContents.map(createSubContent)}</div>;
+const FoodCardsList = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  overflow: hidden;
+
+  height: 557px;
+  transform: ${(props) => `translateX(${props.pxToMove}%`};
+`;
+const CreateSubContent = (content, idx) => {
+  const [onButtonClick, setOnButtonClick] = useState(0);
+  const [distanceToMove, setDistanceToMove] = useState(0);
+  const [prevOnButtonClick, setPrevOnButtonClick] = useState(onButtonClick);
+  const CarouselCardsLength = Array.from(
+    { length: Math.ceil(content.foods.length / 4) },
+    (v, i) => i * 4,
+  );
+
+  useEffect(() => {
+    if (prevOnButtonClick - onButtonClick < 0) {
+      setDistanceToMove(100);
+    }
+  });
+
+  return (
+    <div key={content + idx}>
+      <SubTitle>
+        <Text font={FONT.XLARGE_BOLD}>{content.title}</Text>
+      </SubTitle>
+      <FoodCardsWrap>
+        <LeftArrow>
+          <LeftArrowIcon />
+        </LeftArrow>
+        <FoodCardsList pxToMove={distanceToMove}>
+          {CarouselCardsLength.map((el) => (
+            <FoodCards key={el} foods={content.foods.slice(el, el + 4)} />
+          ))}
+        </FoodCardsList>
+        <RightArrow
+          onClick={() => {
+            setPrevOnButtonClick(onButtonClick);
+            setOnButtonClick(onButtonClick + 1);
+          }}
+        >
+          <RightArrowIcon />
+        </RightArrow>
+      </FoodCardsWrap>
+    </div>
+  );
+};
+
+const SubContents = ({ subContents }) => <div>{subContents.map(CreateSubContent)}</div>;
 export default SubContents;
